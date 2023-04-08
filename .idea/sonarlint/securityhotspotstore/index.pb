60faac810e7fe433aca9f73d8b49d2e70e2a2053
JARO-Hub <juliancitobz13@gmail.com>
2023-04-07 20:52:33 -0400

    init 1
    Provando servidor

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,9 +1,5 @@
 
-9
-	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 D
 assets/normalize.css,3/f/3f54d68279ade2a06c01289396a3a18204a92872
 K
 assets/index-stylesheet.css,d/0/d0c429f00d7ed8c72838a2116dc906f08ace30a5
->
-View/index.php,4/e/4e0345a5eb998b686be7f862b565aea48919d17c
---
+++ View/index.php
@@ -0,0 +1,20 @@
+
+<?php echo "provando"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fronted Store</title>
+    <link rel="stylesheet" href="assets/styles/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,400;1,100;1,300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,400;1,100;1,300&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles/index-stylesheet.css">
+
+</head>
